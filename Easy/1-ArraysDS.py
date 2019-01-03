6e1ed74b411cbcec88540b8a683f1775412956d1
Peter Njenga <peternjengask@gmail.com>
2019-01-03 08:37:35 +0300

    Accepted algorithmic approach to reversal of array

--- Easy/1-ArraysDS.py
+++ Easy/1-ArraysDS.py
@@ -3,13 +3,25 @@
 
 import os
 
+
 # Complete the reverseArray function below.
 def reverseArray(a):
     a.reverse()
-    
+
+
+def reverseArray1(a):
+    left = 0
+    right = len(a) - 1
+
+    while left <= right:
+        a[left], a[right] = a[right], a[left]
+        left += 1
+        right -= 1
+
+    return a
+
+
 if __name__ == '__main__':
-    array = [1, 2, 3, 4]
-    reverseArray(array)
+    array = [1, 2, 3, 4, 5]
+    reverseArray1(array)
     print(array)
-
-
