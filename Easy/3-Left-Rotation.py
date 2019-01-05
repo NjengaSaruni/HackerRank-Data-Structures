fa849d8e7b988530ffe3a488c2d34aee3cc94a70
Peter Njenga <peternjengask@gmail.com>
2019-01-05 09:49:23 +0300

    Accepted queue solution to array solution

--- Easy/3-Left-Rotation.py
+++ Easy/3-Left-Rotation.py
@@ -1,21 +1,11 @@
 def leftRotate(a, r):
-    l = len(a)
-    r = r % l
-    i = r
-
-    if r * 2 > l:
-        while i > 0:
-            j = (l - r + i) % l
-            a[i], a[j] = a[j], a[i]
-            i -= 1
-    else:
-        while i < l:
-            j = (l - r + i) % l
-            a[i], a[j] = a[j], a[i]
-            i += 1
+    for i in range(r):
+        a.append(a.pop(0))
 
 
 if __name__ == '__main__':
     a = [1, 2, 3, 4, 5]
     leftRotate(a, 3)
-    print(a)
+
+    for i in a:
+        print(i, end=" ")
